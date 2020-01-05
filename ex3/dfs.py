6d7a0c55a7164dcf31f668f2743f3417533c78d9
carpicami <58169681+carpicami@users.noreply.github.com>
2020-01-05 17:07:47 +0100

    Update dfs.py

--- ex3/dfs.py
+++ ex3/dfs.py
@@ -1,23 +1,3 @@
-# Copyright 2013, Michael H. Goldwasser
-#
-# Developed for use with the book:
-#
-#    Data Structures and Algorithms in Python
-#    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
-#    John Wiley & Sons, 2013
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import time
 
 def DFS(g, u, discovered):
@@ -143,9 +123,7 @@ def iterative_dfs(g, u,DEBUG=False):
 						
 						#se il while si conclude con questa i allora la condizione non + verificata subito e avviene una stampa un più ?
 						#non andrò in quei nodi quindi incremento
-
 					else:
-
 						new = e.opposite(act)
 						if DEBUG:
 							print("New node " + str(new))
@@ -154,7 +132,6 @@ def iterative_dfs(g, u,DEBUG=False):
 						
 						break
 						"""Che succede se un nodo ha solo blind spot ? Chi è il new ?"""
-
 				else:
 					#setta un back_edge
 					if DEBUG:
@@ -179,7 +156,6 @@ def iterative_dfs(g, u,DEBUG=False):
 						break
 					#questo evita di tornare sempre al nodo in cui mi trovavo prima
 					#di percorrere il primo back edge
-				
 			new = prec
 			going_back = True
 
@@ -189,11 +165,9 @@ def iterative_dfs(g, u,DEBUG=False):
 			#i+=1
 			pass
 
-
 		prec = act
 		act = new
 		act_edge = new_edge
-
 		
 		if DEBUG:
 			time.sleep(1)
