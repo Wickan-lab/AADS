7b1efdfe8db16a40f92cd17f3d12e8a0d93be215
carpicami <58169681+carpicami@users.noreply.github.com>
2020-01-05 17:14:02 +0100

    Update graph.py

--- ex3/graph.py
+++ ex3/graph.py
@@ -1,24 +1,3 @@
-# Copyright 2013, Michael H. Goldwasser
-#
-# Developed for use with the book:
-#
-#    Data Structures and Algorithms in Python
-#    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
-#    John Wiley & Sons, 2013
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 class Graph:
   """Representation of a simple graph using an adjacency map."""
 
@@ -49,8 +28,6 @@ class Graph:
     	return self._dfs_visited == 'visited'
     def set_unexplored(self):
     	self._dfs_visited ='unexplored'
-
-    #if not visted va bene
 
   #------------------------- nested Edge class -------------------------
   class Edge:
